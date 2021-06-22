a23ce4fd88dc8579994ff428c8f6e805c20de0aa
garinerosette <85511487+garinerosette@users.noreply.github.com>
2021-06-22 02:34:44 +0000

    I wrote necessary code to complete parts 2 and 3, using array variables, for loops, and if else statements.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 [![Work in Repl.it](https://classroom.github.com/assets/work-in-replit-14baed9a392b3a25080506f3b7b6d57f295ec2978f6f33ec97e36a161684cbe9.svg)](https://classroom.github.com/online_ide?assignment_repo_id=4877563&assignment_repo_type=AssignmentRepo)
 # Candidate-Testing
+
+let questions = [];
+let correctAnswers = [];
+
+"Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters?	", 
+"(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+
+"What is the minimum crew size for the ISS? "
--- candidate-testing.js
+++ candidate-testing.js
@@ -7,11 +7,11 @@ let candidateName = ""
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? "
-let correctAnswer = "Sally Ride"
-let candidateAnswer = ""
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -22,21 +22,39 @@ candidateName = input.question("Enter your name: ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+//candidateAnswer = input.question(question);
+
+for (let i = 0; i < questions.length; i++) {
+  candidateAnswers[i] = input.question(questions[i]);
+}
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer.toUpperCase() === correctAnswer.toUpperCase()) {
-  console.log("Great job! You are correct! ");
-} else { 
-  console.log("Sorry, not quite. Try again. ");
-}
+//if (candidateAnswer.toUpperCase() === correctAnswer.toUpperCase()) {
+//  console.log("Great job! You are correct! ");
+//} else { 
+ // console.log("Sorry, not quite. Try again. ");
+//}
 
-  let grade
+  let grade = 0
+
+  for (let i = 0; i < candidateAnswers.length; i++) {
+    if (candidateAnswers[i].toUpperCase() === correctAnswers[i].toUpperCase()) {
+      grade++;
+    }
+    console.log(`\nYour answer: ${candidateAnswers[i]} \nCorrect answer: ${correctAnswers[i]}\n`);
+  }
   
+let gradePercent = (grade/5) * 100
+
+if (gradePercent >= 80) {
+  console.log(`Congratulations! Your score is ${gradePercent}%. You passed the test! Welcome aboard, Astronaut ${candidateName}!`) 
+} else {
+console.log(`We regret to inform you that you have FAILED this test. Your score is ${gradePercent}%, but you need at least 80% to pass. Try again next year!`)
+};
 
   return grade;
 }
